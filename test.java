be8373faa4a327b902d28514df03a2f439ea425d
rmw1075 <rmw1075@g.rit.edu>
2019-03-04 12:51:56 -0500

    added for loop

--- test.java
+++ test.java
@@ -2,5 +2,8 @@ public class test{
     public static void main(String[] args) {
         System.out.println("This is a test");
         int hello = 0;
+        for(int a = 0; a < 9; a++){
+            System.out.print(hello);
+        }
     }
 }
