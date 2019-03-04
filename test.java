848638de368fae09bfd1187bcc7e0c7e6c0e029b
rmw1075 <47696363+rmw1075@users.noreply.github.com>
2019-03-04 12:53:32 -0500

    deleted the test

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test{
-    public static void main(String[] args) {
-        System.out.println("This is a test");
-        int hello = 0;
-        for(int a = 0; a < 9; a++){
-            System.out.print(hello);
-        }
-    }
-}
