2f1007c91f90e2cdf7db4c5f5bf9c9caddc318cc
Dporres20 <34166820+Dporres20@users.noreply.github.com>
2019-04-17 15:21:33 -0400

    Made Changes and started GUI and Skeleton merge

--- Hangman/HangMan.java
+++ Hangman/HangMan.java
@@ -7,6 +7,7 @@ public class HangMan
     private ArrayList<String> wordList = new ArrayList<String>();
     private ArrayList<String> solveWord = new ArrayList<String>();
     private String word = "";
+    private String uncovered = "";
     final String DEFAULT = "WordList2.txt";
 
     //Generates the game
@@ -15,6 +16,7 @@ public class HangMan
         if(filename.equals(""))
         {
             String word = setWord("WordList2.txt");
+            new HangManGUI(word);
             System.out.println(word);
             solveWord(word);
             System.exit(0);
@@ -68,40 +70,59 @@ public class HangMan
     //Function that when called will start the solving of the game, if attempts are higher than the body count, 
     public void solveWord(String wrd)
     {
-      for (char c : wrd.toCharArray())
+      String hide = "";      
+      for (char c: wrd.toCharArray())
       {
-         String convert = Character.toString(c);
-         solveWord.add(convert);
+        String ch = Character.toString(c);
+        hide = hide + "-";
+        solveWord.add(ch);
       }
+
+      System.out.println(hide);
       
       for(int i = 0; i < 6; i++)
       {
-         System.out.print("Enter a letter ");
+         System.out.println("Enter a letter ");
          Scanner input = new Scanner(System.in);
          String in = input.nextLine();
-         
-         
          checkLetter(in);
-         
-         
       }
       
       System.out.println(solveWord);
     }//End of solveWord
     
+    //Checks if the letter passed in is inside of the chosen word
     public boolean checkLetter(String wrd)
     {
+
       if(solveWord.contains(wrd))
       {
+         uncovered = "";
          System.out.println("Letter Found");
+         for (int i = 0; i<word.length(); i++)
+         {
+            if(solveWord.get(i) == wrd)
+            {
+                uncovered = uncovered + wrd;
+            }
+            else
+            {
+                uncovered = uncovered + "-";
+            }
+
+         }
+         System.out.println(uncovered);
          return true;
       }
       else
       {
          System.out.println("Letter not found");
+         System.out.println(uncovered);
          return false;
       }
-    }
+
+      
+    }//End of checkLetter
     
 
     public static void main(String[] args)
@@ -110,5 +131,6 @@ public class HangMan
         Scanner input = new Scanner(System.in);
         String in = input.nextLine();
         new HangMan(in);
+        //new HangMan(in);
     }
 }
